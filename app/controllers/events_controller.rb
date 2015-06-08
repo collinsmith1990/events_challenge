b56f2fcb3f32f5df1e84ccb9fc8565a243aea681
CollinSmith <collinsmith1990@gmail.com>
2015-06-08 13:19:12 -0600

    Corrected time format in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,6 @@ class EventsController < ApplicationController
 
   def index
     @tracks = Track.all.order(column_index: :asc)
-    @events = Event.where("start < :date", { date: DateTime.new(2015, 8, 12, 11, 59, 59) })
+    @events = Event.where("start < :date", { date: DateTime.new(2015, 8, 12, 23, 59, 59) })
   end
 end
